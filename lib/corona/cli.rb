66a39cc7c81218caed3ed82fb1d25f7624e3e8d1
Saku Ytti <saku@ytti.fi>
2014-04-13 14:40:44 +0300

    add function to delete rows from db

--- lib/corona/cli.rb
+++ lib/corona/cli.rb
@@ -1,26 +1,56 @@
 module Corona
   class CLI
+    MAX_DELETE = 1
     require 'corona'
     require 'slop'
 
-    def run cidr=@cidr
-      Corona.new :cidr=>cidr
+    def run
+      if @opts[:poll]
+        Corona.new :cidr=>@opts[:poll]
+      elsif @opts[:remove]
+        remove_from_db @opts[:remove]
+      else
+        Corona.new
+      end
     end
 
     private
 
     def initialize
-      args, opts = opts_parse
-      @cidr = args.shift
-      CFG.debug = true if opts[:debug]
+      args, @opts = opts_parse
+      @arg = args.shift
+      CFG.debug = true if @opts[:debug]
     end
 
     def opts_parse
       opts = Slop.parse(:help=>true) do
-        banner 'Usage: corona [cidr]'
-        on 'd', 'debug', 'Debugging on'
+        banner 'Usage: corona [options] [argument]'
+        on 'd',  'debug',      'Debugging on'
+        on 'p=', 'poll',       'Poll CIDR [argument]'
+        on 'r=', 'remove',     'Remove [argument] from DB'
+        on 'm=', 'max-delete', "Maximum number to detel, default #{MAX_DELETE}"
+        on 's',  'simulate',   'Simulate, do not change DB'
       end
       [opts.parse!, opts]
+    end
+
+    def remove_from_db name
+      max_del = @opts['max-delete'] ? @opts['max-delete'] : MAX_DELETE
+      DB.new
+      devs = DB::Device.filter(Sequel.like(:ptr, "%#{name}%")).all
+      if devs.size > max_del.to_i
+        puts 'Too many matching devices:'
+        devs.each do |dev|
+          puts '  %s (%s)' % [dev.ptr, dev.ip]
+        end
+        puts 'Be more specific'
+      else
+        puts 'Deleting records:'
+        devs.each do |dev|
+          puts '  %s (%s)' % [dev.ptr, dev.ip]
+          dev.delete unless @opts[:simulate]
+        end
+      end
     end
 
   end
