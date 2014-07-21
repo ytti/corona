8b5bdac64cad6970cb8b8aa68a0c493a5202427c
Saku Ytti <saku@ytti.fi>
2014-07-21 20:15:46 +0300

    add screenos objectID

--- lib/corona/model.rb
+++ lib/corona/model.rb
@@ -45,15 +45,17 @@ module Corona
       else
         case sysObjectID
         when Regexp.new('^' + Regexp.quote('1.3.6.1.4.1.12356.'))
-          'fortios' # 1.3.6.1.4.1.12356.101.1.10004
+          'fortios'  # 1.3.6.1.4.1.12356.101.1.10004
         when Regexp.new('^' + Regexp.quote('1.3.6.1.4.1.6486.'))
-          'aos'     # 1.3.6.1.4.1.6486.800.1.1.2.1.11.2.2
+          'aos'      # 1.3.6.1.4.1.6486.800.1.1.2.1.11.2.2
         when Regexp.new('^' + Regexp.quote('1.3.6.1.4.1.6027.'))
-          'ftos'    # 1.3.6.1.4.1.6027.1.3.4
-        when Regexp.new('^' + Regexp.quote('1.3.6.1.4.1.9.1.745'))
-          'asa'     # 1.3.6.1.4.1.9.1.745
+          'ftos'     # 1.3.6.1.4.1.6027.1.3.4
         when Regexp.new('^' + Regexp.quote('1.3.6.1.4.1.1588.'))
-          'fabricos'# .1.3.6.1.4.1.1588.2.1.1.1
+          'fabricos '# 1.3.6.1.4.1.1588.2.1.1.1
+        when Regexp.new('^' + Regexp.quote('1.3.6.1.4.1.3224.'))
+          'screenos' # 1.3.6.1.4.1.3224.1.51 (SSG) 1.16 (Netscreen 2k)
+        when Regexp.new('^' + Regexp.quote('1.3.6.1.4.1.9.1.745'))
+          'asa'      # 1.3.6.1.4.1.9.1.745
         else
           'unsupported'
         end
